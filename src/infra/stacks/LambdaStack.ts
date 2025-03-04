f5dbbc551ce3010cebeb47f98b7d02535e3fd868
unknown <sanjeetkumarit@outlook.com>
2025-03-04 15:46:16 +0530

    basic lambda created and test it in cloudFormation view

---
+++ src/infra/stacks/LambdaStack.ts
@@ -0,0 +1,23 @@
+import {  Stack, StackProps } from 'aws-cdk-lib'
+import { Runtime, Code,Function as LambdaFunction} from 'aws-cdk-lib/aws-lambda';
+import { Construct } from 'constructs';
+import { join } from 'path';
+
+
+export class LambdaStack extends Stack {
+
+
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props)
+
+
+        new LambdaFunction(this, 'HelloLambda', {
+            runtime: Runtime.NODEJS_18_X,
+            handler: 'hello.main',
+            code: Code.fromAsset(join(__dirname, '..', '..','services')),
+        });
+
+        
+
+    }
+}
